e1e9100611994f18ed9a82ee63004d9411b9fef3
joslaine2005 <143111885+joslaine2005@users.noreply.github.com>
2024-09-29 16:52:17 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="login.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>tela de login</title>
+ </head>
+<body>
+    <a href="index.html">Voltar</a>
+ <div>
+<h1>login</h1>
+<form action="testLogin.php" method="POST">
+<input type="text" name="email" placeholder="Email">
+<br><br>
+<input type="password" name="senha" placeholder="Senha">
+<br><br>
+<input class="inputSubmit" type="submit" name="submit" value="Enviar">
+</form>
+    </div>
+</body>
+</html>
